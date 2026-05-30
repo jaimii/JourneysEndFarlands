ef56e1ef280025d2c5e21381cf8e9fb9c83c7257
JustJaimii <jancarlodalacat@gmail.com>
2026-05-30 10:23:36 +0800

    Initial commit

---
+++ src/main/kotlin/project/kompass/journeysEndFarlands/FastNoise.kt
@@ -0,0 +1,4 @@
+package project.kompass.journeysEndFarlands
+
+class FastNoise {
+}
---
+++ src/main/kotlin/project/kompass/journeysEndFarlands/JourneysEndFarlands.kt
@@ -0,0 +1,14 @@
+package project.kompass.journeysEndFarlands
+
+import org.bukkit.plugin.java.JavaPlugin
+
+class JourneysEndFarlands : JavaPlugin() {
+
+    override fun onEnable() {
+        // Plugin startup logic
+    }
+
+    override fun onDisable() {
+        // Plugin shutdown logic
+    }
+}
